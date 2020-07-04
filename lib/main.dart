d775443b1f9c4c1de371d01a142e6f19af2f8923
Raquibul Islam <shubha360@outlook.com>
2020-07-04 13:23:13 +0600

    Fixed orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:burgerjoint/Screens/Welcome_Screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MainApp());
@@ -8,6 +9,10 @@ void main() {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: ThemeData.dark(),
       home: WelcomeScreen(),
